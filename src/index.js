242de37b22c6152a60b7df4003b175ca6870c074
oisheemaz <oishee.sde@gmail.com>
2021-10-20 22:40:37 +0530

    Added Authentication uing JWT

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path)
+  next()
+})
+
+// //maintainence mode
+// app.use((req, res, next) => {
+//   res.status(503).send('Site is currently down!')
+// })
+
 app.use(express.json());
 app.use(userRouter)
 app.use(taskRouter)
