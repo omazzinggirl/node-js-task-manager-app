155fa39b5fc819fe7267864591378e1e340d8ac6
oisheemaz <oishee.sde@gmail.com>
2021-10-20 19:29:16 +0530

    Added user login functionality

--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,9 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
+        lowercase:true,
         validate(value) {
             if(!validator.isEmail(value)){
                 throw new Error('Email id is invalid')
@@ -41,13 +43,29 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatched = await bcrypt.compare(password, user.password)
+
+    if(!isMatched){
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+
 userSchema.pre('save', async function (next) {
     const user = this
 
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
-
 
     next()
 })
